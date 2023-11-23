f4a152186c1607e6c79aff68edf20b00b735c723
= <=>
2023-11-23 10:11:36 +0300

    fix: router

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,16 @@
 
+from rest_framework.routers import DefaultRouter
 from users.apps import UsersConfig
 from django.urls import path
+
+from users.serializers import UserSerializer
 
 
 app_name = UsersConfig.name
 
+router = DefaultRouter()
+router.register(r'users', UserSerializer, basename='users')
+
 urlpatterns = [
 
-]
+] + router.urls
